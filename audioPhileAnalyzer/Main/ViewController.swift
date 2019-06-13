6f2fad5f7c4d31ab02a5808093e98d089a03d895
Allison McEntire <amcentire@deloitte.com>
2019-06-13 13:03:50 -0700

    first commit

--- audioPhileAnalyzer/Main/ViewController.swift
+++ audioPhileAnalyzer/Main/ViewController.swift
@@ -13,22 +13,16 @@ import AVFoundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     private var diffDataSource: UITableViewDiffableDataSource<Int, String>!
-    var soundPaths: [String] = []
-    var clapOrCoughClassifier = ClapOrCoughClassifier_1_1()
+    private var clapOrCoughClassifier = ClapOrCoughClassifier_1_1()
+    private let snapshot = NSDiffableDataSourceSnapshot<Int, String>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         let cellId = "CellID"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        
-        // TODO: Replace with sound stream analyzer
-        guard let pathToSnoring = Bundle.main.path(forResource: "testAudio.wav", ofType: nil) else {
-            //TODO: Handle this scenario?
-            return
-        }
-        soundPaths = [pathToSnoring]
         
         diffDataSource = UITableViewDiffableDataSource<Int, String>(tableView: tableView) { (tableView:UITableView, indexPath:IndexPath, model: String) -> UITableViewCell? in
             let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
@@ -41,28 +35,9 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let snapshot = NSDiffableDataSourceSnapshot<Int, String>()
         snapshot.appendSections([0])
         snapshot.appendItems(analyzeAudioFile())
         diffDataSource.apply(snapshot)
-        //borissMagicFunctionOfAwesomePOWER()
-    }
-    
-    //This function was written by Boris Mezhibovskiy
-    //TODO: update so it loads percent return from audio analysis
-    func borissMagicFunctionOfAwesomePOWER() {
-        DispatchQueue.main.asyncAfter(deadline: .now()+2.0) { [weak self] in
-            self?.borissMagicFunctionOfAwesomePOWER()
-        }
-        
-        let currentSnapshot = diffDataSource.snapshot()
-        let randomIndex = Int(arc4random()) % currentSnapshot.numberOfItems
-        let randomId = currentSnapshot.itemIdentifiers[randomIndex]
-        for result in analyzeAudioFile() {
-            currentSnapshot.insertItems([result], afterItem: randomId)
-        }
-        
-        diffDataSource.apply(currentSnapshot)
     }
     
     func analyzeAudioFile() -> [String] {
@@ -85,5 +60,5 @@ class ViewController: UIViewController {
         }
         return resultsArray
     }
-}
 
+}
